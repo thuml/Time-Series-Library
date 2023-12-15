c2f53c2359b23b743bdc9da639b0fc3ac3db1880
wuhaixu2016 <wuhaix2016@163.com>
2023-12-15 10:30:02 +0800

    Update Nonstationary_Transformer.sh

--- scripts/long_term_forecast/ECL_script/Nonstationary_Transformer.sh
+++ scripts/long_term_forecast/ECL_script/Nonstationary_Transformer.sh
@@ -10,7 +10,7 @@ python -u run.py \
   --model_id ECL_96_96 \
   --model $model_name \
   --data custom \
-  --features S \
+  --features M \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 96 \
@@ -34,7 +34,7 @@ python -u run.py \
   --model_id ECL_96_192 \
   --model $model_name \
   --data custom \
-  --features S \
+  --features M \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 192 \
@@ -58,7 +58,7 @@ python -u run.py \
   --model_id ECL_96_336 \
   --model $model_name \
   --data custom \
-  --features S \
+  --features M \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 336 \
@@ -82,7 +82,7 @@ python -u run.py \
   --model_id ECL_96_720 \
   --model $model_name \
   --data custom \
-  --features S \
+  --features M \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 720 \
