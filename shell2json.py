9e461b2001fc7b85deeba3e71618a97313294922
Tianxiang Zhan <166518632@qq.com>
2024-02-21 19:10:31 +0800

    Add a conventor from running script to json

---
+++ shell2json.py
@@ -0,0 +1,131 @@
+import argparse
+import json
+import os
+import shlex
+
+
+def get_filepath(dir_path):
+    def fun(dir_path, list_name):
+        """递归获取目录下（文件夹下）所有文件的路径"""
+        for file in os.listdir(dir_path):  # 获取文件（夹）名
+            file_path = os.path.join(dir_path, file)  # 将文件（夹）名补全为路径
+            if os.path.isdir(file_path):  # 如果是文件夹，则递归
+                fun(file_path, list_name)
+            else:
+                if file_path.endswith('.sh'):
+                    list_name.append(file_path)  # 保存路径
+        return list_name
+
+    res = []
+    fun(dir_path, res)
+    return res
+
+
+parser = argparse.ArgumentParser(description='TimesNet')
+
+# basic config
+parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast',
+                    help='task name, options:[long_term_forecast, short_term_forecast, imputation, classification, anomaly_detection]')
+parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
+parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
+parser.add_argument('--model', type=str, required=True, default='Autoformer',
+                    help='model name, options: [Autoformer, Transformer, TimesNet]')
+
+# data loader
+parser.add_argument('--data', type=str, required=True, default='ETTm1', help='dataset type')
+parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
+parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+parser.add_argument('--features', type=str, default='M',
+                    help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
+parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+parser.add_argument('--freq', type=str, default='h',
+                    help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+# forecasting task
+parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+parser.add_argument('--label_len', type=int, default=48, help='start token length')
+parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
+parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
+
+# inputation task
+parser.add_argument('--mask_rate', type=float, default=0.25, help='mask ratio')
+
+# anomaly detection task
+parser.add_argument('--anomaly_ratio', type=float, default=0.25, help='prior anomaly ratio (%)')
+
+# model define
+parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock')
+parser.add_argument('--num_kernels', type=int, default=6, help='for Inception')
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=7, help='output size')
+parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+parser.add_argument('--factor', type=int, default=1, help='attn factor')
+parser.add_argument('--distil', action='store_false',
+                    help='whether to use distilling in encoder, using this argument means not using distilling',
+                    default=True)
+parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
+parser.add_argument('--embed', type=str, default='timeF',
+                    help='time features encoding, options:[timeF, fixed, learned]')
+parser.add_argument('--activation', type=str, default='gelu', help='activation')
+parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+parser.add_argument('--channel_independence', type=int, default=0,
+                    help='1: channel dependence 0: channel independence for FreTS model')
+parser.add_argument('--conv_kernel', type=tuple, default=(12, 16))
+# optimization
+parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
+parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+parser.add_argument('--des', type=str, default='test', help='exp description')
+parser.add_argument('--loss', type=str, default='MSE', help='loss function')
+parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+# GPU
+parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+
+# de-stationary projector params
+parser.add_argument('--p_hidden_dims', type=int, nargs='+', default=[128, 128],
+                    help='hidden layer dimensions of projector (List)')
+parser.add_argument('--p_hidden_layers', type=int, default=2, help='number of hidden layers in projector')
+
+scripts = get_filepath("./scripts")
+
+for path in scripts:
+    with open(path, 'r') as f:
+        shell = f.read()
+        f.close()
+
+    shell = shlex.split(shell)
+    shell = [shell[i] for i in range(len(shell)) if shell[i] != '\n']
+    shells = []
+    model_name = shell[2].replace("model_name=", "")
+
+    for i in range(len(shell)):
+        if shell[i] == 'python':
+            shells.append(i)
+
+    shells = [shell[shells[i] + 3:shells[i + 1] if i + 1 < len(shells) else len(shell)] for i in range(len(shells))]
+
+    for i in range(len(shells)):
+        try:
+            args = parser.parse_args(shells[i])
+            args.model_name = model_name
+            with open(path.replace(".sh", f"_{i}.json"), 'w') as f:
+                json.dump(args.__dict__, f)
+                f.close()
+        except Exception as e:
+            print(f"Error in {path}")
