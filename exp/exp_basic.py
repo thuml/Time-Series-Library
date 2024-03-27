adc8819c2451eae254e13b8df209dc7272fcd854
weiming.wsy <weiming.wsy@antgroup.com>
2024-03-27 14:47:55 +0800

    add=add TimeMixer and TSMixer

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -2,7 +2,7 @@ import os
 import torch
 from models import Autoformer, Transformer, TimesNet, Nonstationary_Transformer, DLinear, FEDformer, \
     Informer, LightTS, Reformer, ETSformer, Pyraformer, PatchTST, MICN, Crossformer, FiLM, iTransformer, \
-    Koopa, TiDE, FreTS
+    Koopa, TiDE, FreTS, TimeMixer, TSMixer
 
 
 class Exp_Basic(object):
@@ -27,7 +27,9 @@ class Exp_Basic(object):
             'iTransformer': iTransformer,
             'Koopa': Koopa,
             'TiDE': TiDE,
-            'FreTS': FreTS
+            'FreTS': FreTS,
+            'TimeMixer': TimeMixer,
+            'TSMixer': TSMixer
         }
         self.device = self._acquire_device()
         self.model = self._build_model().to(self.device)
