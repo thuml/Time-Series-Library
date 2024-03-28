719c52a00aee307b5ca21d8ae07a1216758156bd
weiming.wsy <weiming.wsy@antgroup.com>
2024-03-28 15:23:30 +0800

    add=add TimeMixer and TSMixer

--- models/TimeMixer.py
+++ models/TimeMixer.py
@@ -300,15 +300,14 @@ class Model(nn.Module):
             x_enc_sampling = down_pool(x_enc_ori)
 
             x_enc_sampling_list.append(x_enc_sampling.permute(0, 2, 1))
-
-            x_mark_sampling_list.append(x_mark_enc_mark_ori[:, ::self.configs.down_sampling_window, :])
-
             x_enc_ori = x_enc_sampling
 
-            x_mark_enc_mark_ori = x_mark_enc_mark_ori[:, ::self.configs.down_sampling_window, :]
+            if x_mark_enc is not None:
+                x_mark_sampling_list.append(x_mark_enc_mark_ori[:, ::self.configs.down_sampling_window, :])
+                x_mark_enc_mark_ori = x_mark_enc_mark_ori[:, ::self.configs.down_sampling_window, :]
 
         x_enc = x_enc_sampling_list
-        x_mark_enc = x_mark_sampling_list
+        x_mark_enc = x_mark_sampling_list if x_mark_enc is not None else None
 
         return x_enc, x_mark_enc
 
