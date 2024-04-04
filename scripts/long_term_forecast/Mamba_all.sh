21ebe5b40c809bc1ba79b81059d9dc14e407df98
frecklebars <popaalex440@gmail.com>
2024-04-04 14:15:58 +0100

    removed tee from mamba all script

--- scripts/long_term_forecast/Mamba_all.sh
+++ scripts/long_term_forecast/Mamba_all.sh
@@ -1,4 +1,4 @@
-./scripts/long_term_forecast/ECL_script/Mamba.sh | tee mamba_all.txt
-./scripts/long_term_forecast/Traffic_script/Mamba.sh | tee mamba_all.txt -a
-./scripts/long_term_forecast/Exchange_script/Mamba.sh | tee mamba_all.txt -a
-./scripts/long_term_forecast/Weather_script/Mamba.sh | tee mamba_all.txt -a
+./scripts/long_term_forecast/ECL_script/Mamba.sh
+./scripts/long_term_forecast/Traffic_script/Mamba.sh
+./scripts/long_term_forecast/Exchange_script/Mamba.sh
+./scripts/long_term_forecast/Weather_script/Mamba.sh
