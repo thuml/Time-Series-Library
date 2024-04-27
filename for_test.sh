5edcec047d99208c02287f423143c9f6abe26dd8
Xiang Xian, Zhang <j087061439@gmail.com>
2024-04-27 20:51:01 +0800

    test: run formosa

--- for_test.sh
+++ for_test.sh
@@ -6,18 +6,18 @@ python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/wind/ \
-  --data_path tpc_RF.csv \
+  --data_path tpc_RFformosa.csv \
   --model_id wind_12_12 \
   --model $model_name \
   --data custom \
   --features MS \
   --seq_len 12 \
-  --label_len 5 \
-  --pred_len 12 \
+  --label_len 12 \
+  --pred_len 96 \
   --e_layers 3 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in 7 \
+  --enc_in 13 \
   --dec_in 7 \
   --c_out 1 \
   --des 'Exp' \
