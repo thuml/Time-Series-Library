e854e8f4a9259fad3622ee9893fe11637f1e52f7
Xiang Xian, Zhang <j087061439@gmail.com>
2024-04-27 20:55:57 +0800

    fix: script typo

--- for_test.sh
+++ for_test.sh
@@ -6,7 +6,7 @@ python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/wind/ \
-  --data_path tpc_RFformosa.csv \
+  --data_path formosa.csv \
   --model_id wind_12_12 \
   --model $model_name \
   --data custom \
