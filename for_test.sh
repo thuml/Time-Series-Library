a382f97543c5c429ae1b0395e41c32af326c294b
johnny890122 <j087061439@gmail.com>
2024-05-08 11:47:42 +0800

    chore: collect iTransformer exp dat

--- for_test.sh
+++ for_test.sh
@@ -1,13 +1,13 @@
 export CUDA_VISIBLE_DEVICES=0
 
-model_name=TimesNet
+model_name=iTransformer
 
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/wind/ \
   --data_path formosa_wrf.csv \
-  --model_id wind_12_12 \
+  --model_id wind_12_12_wrf \
   --model $model_name \
   --data custom \
   --features MS \
@@ -17,8 +17,8 @@ python -u run.py \
   --e_layers 3 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in 13 \
-  --dec_in 13 \
+  --enc_in 27 \
+  --dec_in 27 \
   --c_out 1 \
   --des 'Exp' \
   --d_model 64\
