56108fb7363814be37c136b88810cb576142bcd5
johnny890122 <j087061439@gmail.com>
2024-07-01 11:43:33 +0800

    update

--- for_test.sh
+++ for_test.sh
@@ -1,24 +1,24 @@
 export CUDA_VISIBLE_DEVICES=0
 
-model_name=TimesNet
+model_name=iTransformer
 
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/wind/ \
-  --data_path formosa_wrf.csv \
-  --model_id wind_12_12_wrf \
+  --data_path formosa_wrf_short.csv \
+  --model_id wind_12_12_short_wrf \
   --model $model_name \
   --data custom \
   --features MS \
   --seq_len 12 \
   --label_len 12 \
-  --pred_len 96 \
+  --pred_len 12 \
   --e_layers 3 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in 27 \
-  --dec_in 27 \
+  --enc_in 21 \
+  --dec_in 21 \
   --c_out 1 \
   --des 'Exp' \
   --d_model 64\
