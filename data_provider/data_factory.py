6d1c2fcfd09d5685ba5f1a1031a945f443b7057b
WebberShaw <webber@tech-webs.com>
2024-08-13 11:12:37 +0800

    fixed data_factory.py which will shuffle test dataset for classification task

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -23,7 +23,7 @@ def data_provider(args, flag):
     Data = data_dict[args.data]
     timeenc = 0 if args.embed != 'timeF' else 1
 
-    shuffle_flag = False if flag == 'test' else True
+    shuffle_flag = False if (flag == 'test' or flag == 'TEST') else True
     drop_last = False
     batch_size = args.batch_size
     freq = args.freq
