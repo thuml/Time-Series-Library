e6a1f393f891339575adc89c56b7e719560baaf1
82anonymous42 <82anonymous42@gmail.com>
2024-11-10 11:48:01 +0000

    fix: rm np.array from m4 data provider

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -338,9 +338,11 @@ class Dataset_M4(Dataset):
             dataset = M4Dataset.load(training=True, dataset_file=self.root_path)
         else:
             dataset = M4Dataset.load(training=False, dataset_file=self.root_path)
-        training_values = np.array(
-            [v[~np.isnan(v)] for v in
-             dataset.values[dataset.groups == self.seasonal_patterns]])  # split different frequencies
+        training_values = [
+	    v[~np.isnan(v)] for v in 
+	    dataset.values[dataset.groups == self.seasonal_patterns]
+	] # split different frequencies
+	
         self.ids = np.array([i for i in dataset.ids[dataset.groups == self.seasonal_patterns]])
         self.timeseries = [ts for ts in training_values]
 
