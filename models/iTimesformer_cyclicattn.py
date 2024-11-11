4854c0846f798c17efca2817609cc49e29c5fd07
lorenzobrigato <lory_briga@hotmail.it>
2024-11-11 18:07:57 +0100

    Integrated decoupled attention in model

---
+++ models/iTimesformer_cyclicattn.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.Transformer_EncDec import Encoder, CyclicEncoderLayer
+from layers.SelfAttention_Family import FullAttention, AttentionLayer
+from layers.Embed import DataEmbedding_inverted
+from layers.iTimesformer_Periodicity import PeriodicityReshape, PositionalEncoding
+from iTimesformer import Model as iTimesformerModel
+
+
+class Model(iTimesformerModel):
+
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.main_cycle = configs.main_cycle
+        if configs.n_cycles == -1:
+            self.n_cycles = configs.seq_len // self.main_cycle
+        self.n_cycles = configs.n_cycles # Number of historic cycles 
+        self.n_features = configs.c_out
+        self.task_name = configs.task_name
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        # Embedding
+        self.enc_embedding = DataEmbedding_inverted(self.main_cycle, configs.d_model, configs.embed, configs.freq,
+                                                    configs.dropout)
+        self.positional_encoding = PositionalEncoding(configs.d_model)
+        self.periodicity_reshape = PeriodicityReshape(self.main_cycle)
+        # Encoder
+        self.encoder = Encoder(
+            [
+                CyclicEncoderLayer(
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=False), configs.d_model, configs.n_heads),
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=False), configs.d_model * configs.c_out, configs.n_heads),
+                    configs.d_model,
+                    configs.n_cycles,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        )
+        # Decoder
+        if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
+            self.projection = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+        if self.task_name == 'imputation':
+            self.projection = nn.Linear(configs.d_model, configs.seq_len//self.n_cycles, bias=True) # divide by n_cycles to match the shaping strategy by main_cycle
+        if self.task_name == 'anomaly_detection':
+            self.projection = nn.Linear(configs.d_model, configs.seq_len//self.n_cycles, bias=True) # divide by n_cycles to match the shaping strategy by main_cycle
+        if self.task_name == 'classification':
+            self.act = F.gelu
+            self.dropout = nn.Dropout(configs.dropout)
+            self.projection = nn.Linear(configs.d_model * configs.enc_in, configs.num_class)
+
