dfdb2d0548bc4e5d4c76f272d17ac7d53af305b1
Vahid Naghashi <35852666+VahidNaghashi@users.noreply.github.com>
2025-01-30 22:19:28 -0500

    Create Weather.sh

---
+++ scripts/Weather.sh
@@ -0,0 +1,82 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=MultiPatchFormer
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --d_model 256\
+  --d_ff 512\
+  --itr 1
+
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 192 \
+  --e_layers 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --d_model 256\
+  --d_ff 512\
+  --itr 1
+
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 336 \
+  --e_layers 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --d_model 256\
+  --d_ff 512\
+  --itr 1
+
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 720 \
+  --e_layers 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --d_model 256\
+  --d_ff 512\
+  --itr 1
