40da06fef6e703b62c86bbea1c3d798f15a73fcb
wuhaixu2016 <wuhaix2016@163.com>
2025-03-07 10:34:02 +0800

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -85,9 +85,9 @@ def visual(true, preds=None, name='./pic/test.pdf'):
     Results visualization
     """
     plt.figure()
-    plt.plot(true, label='GroundTruth', linewidth=2)
     if preds is not None:
         plt.plot(preds, label='Prediction', linewidth=2)
+    plt.plot(true, label='GroundTruth', linewidth=2)
     plt.legend()
     plt.savefig(name, bbox_inches='tight')
 
